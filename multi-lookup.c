166abbaef823db5eb84e0c493cd3bc3454fb4891
Brian E. Peterson <bepetersn@gmail.com>
2020-10-24 22:46:38 -0600

    Use mt_cirque to implement file_arr too

--- multi-lookup.c
+++ multi-lookup.c
@@ -5,18 +5,16 @@
 #include "mt-cirque.c"
 #include "util.h"
 
-#define ARRAY_SIZE 20
-#define MAX_INPUT_FILES 10
+#define MAX_INPUT_FILES 10      // TODO: Enforce
+#define MAX_REQUESTOR_THREADS 5 // TODO: Enforce
 #define MAX_DOMAIN_NAME_LENGTH 1025
-#define MAX_REQUESTOR_THREADS 5
 
 void *resolver_thread_func(void *param);
 void *requester_thread_func(void *param);
-int next_ptr = 0;
 
 struct ThreadArgs
 {
-    char *file_arr[MAX_INPUT_FILES];
+    mt_cirque *file_arr;
     mt_cirque *shared_buff;
     FILE *log;
 };
@@ -25,16 +23,23 @@ int main(int argc, char *argv[])
 {
     // create an array of files
     // create shared_buffer
-    // create a parsing log
+    // create logs
     //
 
     puts("in main");
 
     // create & serialize pthread_create args
+    int i;
     pthread_t tid1, tid2;
     struct ThreadArgs *thread_args = malloc(sizeof(struct ThreadArgs));
-    thread_args->file_arr[0] = argv[1];
+
+    thread_args->file_arr = make_mt_cirque();
     thread_args->shared_buff = make_mt_cirque();
+    for (i = 0; i < argc - 1; i++)
+    {
+        printf("queuing %s\n", argv[i + 1]);
+        mt_cirque_push(thread_args->file_arr, argv[i + 1]);
+    }
 
     // create a thread for parsing
     if (pthread_create(
@@ -56,9 +61,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
     pthread_join(tid2, NULL);
-
     printf("Parent quiting\n");
-
     return 0;
 }
 
@@ -69,32 +72,35 @@ void *requester_thread_func(void *param)
     // Deserialize args
     struct ThreadArgs **argsp = param;
     struct ThreadArgs *args = *argsp;
-
-    // Given a file ...
-    char *filepath = args->file_arr[0];
+    char *filepath;
     char *domain = malloc(MAX_DOMAIN_NAME_LENGTH);
     int push_error;
 
-    // open it ...
-    FILE *fp = fopen(filepath, "r");
-    if (fp == NULL)
+    // While files_arr is not empty, take from files_arr
+    while ((filepath = mt_cirque_pop(args->file_arr)) != NULL)
     {
-        printf("in requester: failed to open file: %s\n", filepath);
-        exit(1);
-    }
-    printf("in requester: opened file '%s'\n", args->file_arr[0]);
 
-    // Read lines from the file repeatedly
-    while (fgets(domain, MAX_DOMAIN_NAME_LENGTH, fp) != NULL &&
-           push_error == 0)
-    {
-        // Remove any newlines that may or may not exist
-        domain[strcspn(domain, "\r\n")] = 0; // (MT-safe)
-        printf("in requester: got line from file: %s\n", domain);
+        // open it ...
+        FILE *fp = fopen(filepath, "r");
+        if (fp == NULL)
+        {
+            printf("in requester: failed to open file: %s\n", filepath);
+            exit(1);
+        }
+        printf("in requester: opened file '%s'\n", filepath);
 
-        // .. and add each as an entry into the shared buffer
-        push_error = mt_cirque_push(args->shared_buff, domain);
-        printf("in requester: added to shared buffer? ...: %d\n\n", push_error);
+        // Read lines from the file repeatedly (MT-safe)
+        while (fgets(domain, MAX_DOMAIN_NAME_LENGTH, fp) != NULL &&
+               push_error == 0)
+        {
+            // Remove any newlines that may or may not exist
+            domain[strcspn(domain, "\r\n")] = 0; // (MT-safe)
+            printf("in requester: got line from file: %s\n", domain);
+
+            // .. and add each as an entry into the shared buffer
+            push_error = mt_cirque_push(args->shared_buff, domain);
+            printf("in requester: added to shared buffer? : %d\n\n", push_error);
+        }
     }
     printf("in requester: quiting\n");
     return 0;
