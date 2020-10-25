603aebdaa35e58126a0b6e11457f5cc5c07eed34
Brian E. Peterson <bepetersn@gmail.com>
2020-10-24 22:45:59 -0600

    Fix popping from cirque

--- mt-cirque.c
+++ mt-cirque.c
@@ -71,32 +71,36 @@ int mt_cirque_push(mt_cirque *q, char *str)
 
 char *mt_cirque_pop(mt_cirque *q)
 {
+    char *popped;
+    mt_cirque_display(q);
     if (q->tail == UNINITIALIZED)
     {
         // Failure, because we can't pop
         // from an empty queue
         return NULL;
     }
-    else if (q->head == q->tail)
-    {
-        // Uninitalize the tail
-        // to put the queue in a state
-        // of being empty, the inverse
-        // of the push functionality above
-        q->tail = UNINITIALIZED;
-    }
     else
     {
-        // We pop from the head of the queue; we
-        // achieve this by gradually shifting its
-        // head forward relative to 0 as
-        // values are popped. This has the desirable
-        // property that we can pop and push independently
-        // from the different sides AND we don't have to
-        // reinitialize or grow the queue to accommodate
-        // more than its size elements over time
-        q->head++;
+        popped = strdup(q->data[q->head]);
+        if (q->head == q->tail)
+        {
+            // Uninitalize the tail
+            // to put the queue in a state
+            // of being empty, the inverse
+            // of the push functionality above
+            q->tail = UNINITIALIZED;
+        }
+        else
+        {
+            // We pop from the head of the queue;
+            // This has the desirable property that we can
+            // pop and push independently from the different
+            // sides AND we don't have to reinitialize or
+            // grow the queue to accommodate more than its
+            // size elements over time
+            q->head = (q->head + 1) % MAX_QUEUE_CAPACITY;
+        }
+        q->count--;
+        return popped;
     }
-    q->count--;
-    return strdup(q->data[q->head]);
 }
